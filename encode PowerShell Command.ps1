b4e17ab7af02344543c8484758fd204fbf6d4c17
bradhawkins85 <bradhawkins@gmail.com>
2024-09-11 12:48:17 +1000

    Create encode PowerShell Command.ps1

---
+++ encode PowerShell Command.ps1
@@ -0,0 +1,43 @@
+$Name = "Do Encode"
+
+$Command = @'
+
+function Import-SyncroModule {
+    param (
+        #Defaults to the UUID of local system but you can provide the UUID of Any other Syncro Asset instead.
+        $UUID
+    )
+
+    # Set up $env: vars for Syncro Module
+    if($env:SyncroModule -match '^\s*$'){
+        $SyncroRegKey = Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\RepairTech\Syncro' -Name uuid, shop_subdomain
+        $env:RepairTechFilePusherPath  = 'C:\ProgramData\Syncro\bin\FilePusher.exe'
+        $env:RepairTechKabutoApiUrl    = 'https://rmm.syncromsp.com'
+        $env:RepairTechSyncroApiUrl    = 'https://{subdomain}.syncroapi.com'
+        $env:RepairTechSyncroSubDomain = $SyncroRegKey.shop_subdomain
+        $env:RepairTechUUID            = if($UUID -match '^\s*$'){ $SyncroRegKey.uuid } else {$UUID}
+        $env:SyncroModule              = "$env:ProgramData\Syncro\bin\module.psm1"
+    }
+    if ((Test-Path -Path $env:SyncroModule) -and ($PSVersionTable.PSVersion -ge [system.version]'4.0')) {
+        Import-Module -Name $env:SyncroModule -WarningAction SilentlyContinue
+    } else {
+        if ($PSVersionTable.PSVersion -lt [system.version]'4.0'){Write-Warning "$($PSVersionTable.PSVersion) is not compatible with SyncroModule"}
+        [Environment]::SetEnvironmentVariable('SyncroModule',$null)
+    }
+}
+
+Import-SyncroModule
+
+Rmm-Alert -Category 'USS - Test Connectivity' -Body 'USS - Test Connectivity Requested'
+
+
+'@
+
+$EncodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($Command))
+
+$outcommand = "powershell.exe -EncodedCommand " + $EncodedCommand
+
+Out-File -FilePath "C:\Temp\Powershell\$Name.ps1" -Force -InputObject $command
+Out-File -FilePath "C:\Temp\Powershell\$Name-Encoded.txt" -Force -InputObject $outcommand
+
+
